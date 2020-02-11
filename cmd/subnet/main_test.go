68c3e964ff16cb94e993a1be26f6f6e2de7a9ea0
Ethan Mosbaugh <ethan@replicated.com>
2020-02-11 18:37:32 +0000

    Add debug logs to subnet command

--- cmd/subnet/main_test.go
+++ cmd/subnet/main_test.go
@@ -86,6 +86,17 @@ func TestFindAvailableSubnet(t *testing.T) {
 			},
 			wantErr: true,
 		},
+		{
+			name: "none available 10.17",
+			args: args{
+				subnetSize:  22,
+				subnetRange: mustParseCIDR("10.17.0.0/16"),
+				routes: []netlink.Route{
+					makeRoute("10.17.0.0", 16),
+				},
+			},
+			wantErr: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
